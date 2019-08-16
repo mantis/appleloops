7f84b362b61a1555fe82763be9e800dde22aaad8
carlashley <carl@carlashley.com>
2019-08-16 19:29:09 +1000

    Include zipapp'd file in basedir of repo

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # If you have python3, then you can run this
 if [ -f /usr/local/bin/python3 ]; then
     /usr/local/bin/python3 -m zipapp src --compress --output dist/zipapp/usr/local/bin/appleloops --python='/usr/bin/env python'
+    /usr/local/bin/python3 -m zipapp src --compress --output appleloops --python='/usr/bin/env python'
     /usr/bin/make
 else
     echo "Python 3 is required to use zipapp. Exiting."
